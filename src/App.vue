d498d9e9bafa33569d8626e06f3462a49580e1e5
Patryk Borowiec <56939183+Zefir12@users.noreply.github.com>
2024-03-30 03:45:02 +0100

    fix(): cleanup

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ import Toast from "primevue/toast";
 <!-- 
     https://primevue.org/       for components
     https://vuejs.org/          for vue
+    https://vueschool.io/articles/vuejs-tutorials/eslint-and-prettier-with-vite-and-vue-js-3/   fro setting up linter, maybe setting up linter errors in browser later
 -->
 
 <template>
--- src/components/weight/store/weightStore.ts
+++ src/components/weight/store/weightStore.ts
@@ -3,15 +3,12 @@ import { ref } from "vue";
 import { addNewWeightToDb } from "../../../lib/supabase/services/supabaseWeightService";
 import { useToast } from "primevue/usetoast";
 
-
 export const useWeightStore = defineStore("weightStore", () => {
-    const toast = useToast()
-    const weightToAdd = ref(70.1)
-
-    
+    const toast = useToast();
+    const weightToAdd = ref(70.1);
 
     async function addNewWeight() {
-        await addNewWeightToDb(weightToAdd.value)
+        await addNewWeightToDb(weightToAdd.value);
         toast.add({ severity: "success", summary: "Succes", detail: "Succesfully added new weight", life: 3000 });
     }
 
