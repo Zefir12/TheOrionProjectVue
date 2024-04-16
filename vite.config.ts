370b4d9cb91e60a0357f5edc206da5f2b745ac65
Patryk Borowiec <56939183+Zefir12@users.noreply.github.com>
2024-04-16 23:55:27 +0200

    feat(app): set up path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+            "@assets": path.resolve(__dirname, "./src/assets"),
+            "@components": path.resolve(__dirname, "./src/components")
+        }
+    },
     plugins: [vue()],
     base: "/TheOrionProjectVue/",
     preview: {
-        port: 3999,
+        port: 3999
     },
     server: {
-        port: 3999,
-    },
+        port: 3999
+    }
 });
