61f3defe34064f626ee4d18fea8538b6692ed0fb
Patryk Borowiec <56939183+Zefir12@users.noreply.github.com>
2024-04-16 22:36:25 +0200

    feat(app): save before trying to add linter

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-import MySidebar from "./components/sidebar/MySidebar.vue";
-import Toast from "primevue/toast";
+import MySidebar from "./components/sidebar/MySidebar.vue"
+import Toast from "primevue/toast"
 </script>
 
-<!-- 
+<!--
     https://primevue.org/       for components
     https://vuejs.org/          for vue
     https://vueschool.io/articles/vuejs-tutorials/eslint-and-prettier-with-vite-and-vue-js-3/   fro setting up linter, maybe setting up linter errors in browser later
+    used this to migrating from npm to pnpm ->  https://britishgeologicalsurvey.github.io/development/migrating-from-npm-to-pnpm/
 -->
 
 <template>
