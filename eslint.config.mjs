2a6eaf9f7422a695ada10534f917add32b579cdf
Patryk Borowiec <56939183+Zefir12@users.noreply.github.com>
2024-04-16 23:18:08 +0200

    fuck the new eslint has no ifne yet whatsoever

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import eslintJsPlugin from "@eslint/js";
+
+export default [
+    {
+        rules: eslintJsPlugin.configs.recommended.rules
+    },
+    {
+        files: ["**/*.ts"],
+        languageOptions: {
+            parser: tsParser,
+            globals: globals.node
+        },
+        plugins: {
+            "@typescript-eslint": tsPlugin
+        },
+        rules: {
+            ...tsPlugin.configs.recommended.rules,
+            "no-console": "error"
+        }
+    },
+    {
+        files: ["test/**/*.ts"],
+        languageOptions: {
+            globals: globals.mocha
+        }
+    }
+];
